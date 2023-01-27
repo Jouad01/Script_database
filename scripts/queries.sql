7fd1b35ce293cdeffd133719af778dd92fb9c30d
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-01-27 20:01:39 +0100

    style: delete comments of queries

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -14,12 +14,9 @@ SELECT * FROM bikes WHERE price between 5000 and 9000;
 
 SELECT avg(price) FROM bikes;
 
--- cuantas veces se repite una tienda en el client
 SELECT shop_id, COUNT(*) as num_ocurrencias FROM client group by shop_id having num_ocurrencias > 1;
 
 (SELECT brand, price FROM bikes WHERE brand = 'Specialized' and year= 2022) intersect (SELECT brand, price FROM bikes WHERE brand = 'Specialized' and price > 10000);
-
--- JOIN entre tablas "bikes", "shops" y "booking", para obtener información relacionada entre estas 
 
 SELECT Bikes.model, Shops.shop, Client.name, Client.lastname, Booking.start_date, Booking.end_date
 FROM Bikes
